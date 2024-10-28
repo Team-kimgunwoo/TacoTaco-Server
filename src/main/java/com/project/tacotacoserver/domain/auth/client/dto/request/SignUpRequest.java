5fa495eed8aa7e2f058f2f9bc983941ced7b95c7
lgwk42 <luisgunkim@gmail.com>
2024-10-28 13:41:51 +0900

    :memo: :: sign up req fix

--- src/main/java/com/project/tacotacoserver/domain/auth/client/dto/request/SignUpRequest.java
+++ src/main/java/com/project/tacotacoserver/domain/auth/client/dto/request/SignUpRequest.java
@@ -9,8 +9,5 @@ public record SignUpRequest(
         @Email
         String email,
         String name,
-        @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "정해진 핸드폰 양식을 따라주세요 (010-0000-0000)")
-        String phoneNumber,
-        String password,
-        String imageUrl
+        String password
 ){}
