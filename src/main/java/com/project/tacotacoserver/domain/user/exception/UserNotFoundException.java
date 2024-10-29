92ab7910355cc552e1fb40014b0d6638173eeb06
lgwk42 <luisgunkim@gmail.com>
2024-10-29 13:53:33 +0900

    :bulb: :: comment delete

--- src/main/java/com/project/tacotacoserver/domain/user/exception/UserNotFoundException.java
+++ src/main/java/com/project/tacotacoserver/domain/user/exception/UserNotFoundException.java
@@ -3,7 +3,6 @@ package com.project.tacotacoserver.domain.user.exception;
 import com.project.tacotacoserver.domain.user.exception.error.UserError;
 import com.project.tacotacoserver.global.exception.BusinessException;
 
-/**user 404 exception*/
 public class UserNotFoundException extends BusinessException {
 
     public static final UserNotFoundException EXCEPTION = new UserNotFoundException();
