c98e6547857774a5a04ac8fa8949f5d59684da6e
lgwk42 <luisgunkim@gmail.com>
2024-10-29 12:01:44 +0900

    :sparkles: :: geo req, res add

---
+++ src/main/java/com/project/tacotacoserver/domain/geo/dto/request/GeoRequest.java
@@ -0,0 +1,5 @@
+package com.project.tacotacoserver.domain.geo.dto.request;
+
+public record GeoRequest(
+        String ip
+){}
---
+++ src/main/java/com/project/tacotacoserver/domain/geo/dto/response/GeoResponse.java
@@ -0,0 +1,27 @@
+package com.project.tacotacoserver.domain.geo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class GeoResponse {
+
+    private String requestId;
+    private String returnCode;
+    private String country;
+    private String code;
+    private String r1;
+    private String r2;
+    private String r3;
+    private int lat;
+    private int lng;
+    private String net;
+
+}
